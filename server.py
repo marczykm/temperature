3fa43c1ac684b0ae4838b6238141da31b3304ed5
Marcin Marczyk <m.l.marczyk@gmail.com>
2016-08-13 21:54:43 +0200

    api for current temperature

--- server.py
+++ server.py
@@ -1,13 +1,21 @@
 from flask import Flask
 from flask import render_template
+from flask import jsonify
+
+import time
 import temperature
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def index():
 	temp = str(temperature.read_temp())
 	return render_template('index.html', temp=temp)
+
+@app.route("/api/temperature")
+def temperature_json():
+	temp = temperature.read_temp()
+	return jsonify(timestamp=int(time.time()),temperature=temp)
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=80)
